c882d7b7cbb8f2268e6c5a256d6694d9dfcac9d3
John Bleichert <omortis@outlook.com>
2023-01-23 10:04:57 -0500

    q6, q7

--- ps01.py
+++ ps01.py
@@ -12,12 +12,12 @@ os.system("cls")
 # ---------------------------------------------------------------------
 
 
-def area_of_trapezoid(a, b, h):
-    return (h / 2)*(a + b)
+# def area_of_trapezoid(a, b, h):
+#     return (h / 2)*(a + b)
 
 
-print(area_of_trapezoid(3, 3, 5))
-print(area_of_trapezoid(3, 4, 5))
+# print(area_of_trapezoid(3, 3, 5))
+# print(area_of_trapezoid(3, 4, 5))
 
 # Question 4
 #
@@ -30,12 +30,12 @@ print(area_of_trapezoid(3, 4, 5))
 # ---------------------------------------------------------------------
 
 
-def c_to_f(c):
-    return int((9/5)*c + 32)
+# def c_to_f(c):
+#     return int((9/5)*c + 32)
 
 
-print(c_to_f(22))
-print(c_to_f(0))
+# print(c_to_f(22))
+# print(c_to_f(0))
 
 # Question 5
 #
@@ -47,11 +47,48 @@ print(c_to_f(0))
 # no need to check the sign (-4 / 2 = -2 so -4 is even)
 
 
-def even_or_odd(x):
-    if x % 2 > 0:
-        return "The number {} is odd.".format(x)
-    else:
-        return "The number {} is even.".format(x)
+# def even_or_odd(x):
+#     if x % 2 > 0:
+#         return "The number {} is odd.".format(x)
+#     else:
+#         return "The number {} is even.".format(x)
 
 
-even_or_odd(12)
+# even_or_odd(12)
+
+# Question 6
+#
+# Write a Python function draw(n) that constructs the following pattern
+# using the print function and a nested for loop. Here is what the
+# output should look like for 𝑛=5, but with less space between lines.
+# ---------------------------------------------------------------------
+
+
+# def draw_2(n):
+#     adder = 1
+#     y = 1
+#     for x in range((2 * n) - 1):
+#         print('*' * y)
+#         if x >= n-1:
+#             adder = -1
+#         y += adder
+
+
+# draw_2(5)
+
+# Question 7
+# Write a function that takes in an integer 𝑛 and computes 𝑛!. Do this
+# without recursion.
+# ---------------------------------------------------------------------
+
+
+def factorial_iter(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    return fact
+
+
+print(factorial_iter(6))
+print(factorial_iter(7))
+print(factorial_iter(10))
