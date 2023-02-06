bd8d91af3512babef635dff7c10fb581a61bf5ed
John Bleichert <omortis@outlook.com>
2023-02-05 23:01:58 -0500

    working on 5 & 6

---
+++ lec6-live/ex5.py
@@ -0,0 +1,19 @@
+import os
+
+if os.name == "posix":
+    os.system("clear")
+else:
+    os.system("cls")
+
+# Exercise 5: Challenge
+#
+# Write a function that takes in a list of 3 numbers and returns a
+# list of all possible permutations of that list.
+#
+# You can do this by brute force and but also try using recursion. If
+# you figure out a recursive solution, try to make it work for a
+# list of 𝑛 numbers.
+
+
+def listPermutations():
+    return [1, 2]
---
+++ lec6-live/ex6.py
@@ -0,0 +1,13 @@
+import os
+
+if os.name == "posix":
+    os.system("clear")
+else:
+    os.system("cls")
+
+# Exercise 5 BONUS:
+#
+# Using the function from Exercise 4 Challenge can you compose
+# these functions to compute the average of a list under all of
+# it's permutations?
+
