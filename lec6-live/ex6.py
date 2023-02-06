72b2b029a9684b3969594b4edc66ef1cab69019c
John Bleichert <omortis@outlook.com>
2023-02-06 10:07:25 -0500

    finished ex6

--- lec6-live/ex6.py
+++ lec6-live/ex6.py
@@ -11,3 +11,43 @@ else:
 # these functions to compute the average of a list under all of
 # it's permutations?
 
+
+def listAverage(listOfLists):
+
+    averageList = []
+    for i in range(len(listOfLists[0])):
+        averageList.append(0)
+        for j in range(len(listOfLists)):
+            averageList[i] += listOfLists[j][i]
+
+    # averageList contains totals - need to divide to get average
+    returnList = [x//len(listOfLists) for x in averageList]
+
+    return returnList
+
+
+def bruteForceReverse(inc):
+
+    returnList = []
+
+    for i in range(len(inc)):
+
+        # shallow copy
+        local = inc[:]
+        rev = inc[:]
+
+        returnList.append(local)
+        rev.reverse()
+        returnList.append(rev)
+
+        current = inc[0]
+
+        inc.pop(0)
+        shiftList = inc[:]  # copy, not assign ref
+        shiftList.append(current)
+        inc = shiftList[:]
+
+    return returnList
+
+
+assert listAverage(bruteForceReverse([1, 2, 3])) == [2, 2, 2]
