5140be52ba27ef110d840573ead6c54990dbdbc5
John Bleichert <omortis@outlook.com>
2023-02-08 08:20:30 -0500

    working on kth derivative

--- lec7-live/ex4.py
+++ lec7-live/ex4.py
@@ -23,8 +23,31 @@ print(derivative(f, 0.001)(.5))
 type(derivative(f, 0.001))
 
 
+# Question 4: Part B
+
+# Write a function kthDerivative(f,dx,k) that takes in a function f, a step-size dx
+# and a non-negative integer k and returns the 𝑘𝑡ℎ derivative of 𝑓 with stepsize 𝑑𝑥.
+
+# I've started this code below for you. HINT: You are free to call the derivative function above.
+
+# explicit memoization
+orderOfDerivative = 0
+
+
 def kthDerivative(f, dx, k):
     """Assumes f is a function, dx is a positive float, k is a non-negative int.
     Returns a new function that is an approximation of the kth derivative of f, using step-size dx."""
 
-    return None
+    orderOfDerivative += 1
+
+    if derivative(f, dx) == 0 or orderOfDerivative == k:
+        return f
+    else:
+        return derivative(f, dx)
+
+
+def g(x):
+    return x**4
+
+
+print("g(10) = {}".format(str(g(10))))
