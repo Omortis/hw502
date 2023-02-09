93161e6f12ebbeb60b69164a7b255aeeb1e5ecbc
John Bleichert <omortis@outlook.com>
2023-02-08 20:49:02 -0500

    lec7 complete

--- lec7-live/ex4.py
+++ lec7-live/ex4.py
@@ -19,8 +19,8 @@ def f(x):
     return x**2
 
 
-print(derivative(f, 0.001)(.5))
-type(derivative(f, 0.001))
+# print(derivative(f, 0.001)(.5))
+# type(derivative(f, 0.001))
 
 
 # Question 4: Part B
@@ -34,20 +34,51 @@ type(derivative(f, 0.001))
 orderOfDerivative = 0
 
 
-def kthDerivative(f, dx, k):
-    """Assumes f is a function, dx is a positive float, k is a non-negative int.
-    Returns a new function that is an approximation of the kth derivative of f, using step-size dx."""
+# def kthDerivativeRec(f, dx, k):
+#     """Assumes f is a function, dx is a positive float, k is a non-negative int.
+#     Returns a new function that is an approximation of the kth derivative of f, using step-size dx."""
 
-    orderOfDerivative += 1
-
-    if derivative(f, dx) == 0 or orderOfDerivative == k:
-        return f
-    else:
-        return derivative(f, dx)
+#     if derivative(f, dx) == 0 or orderOfDerivative == k:
+#         return f
+#     else:
+#         return derivative(f, dx)
 
 
 def g(x):
     return x**4
 
 
-print("g(10) = {}".format(str(g(10))))
+def kthDerivative(f, dx, k):
+    """Assumes f is a function, dx is a positive float, k is a non-negative int.
+    Returns a new function that is an approximation of the kth derivative of f, 
+    using step-size dx."""
+
+    f_prime = derivative(f, dx)
+    if k == 0:
+        return f
+    else:
+        return kthDerivative(f_prime, dx, k-1)
+
+
+print("g(10)    = {}".format(str(g(10))))
+
+g1 = derivative(g, 0.001)
+print("g'(10)   = {}".format(g1(10)))
+
+g2 = derivative(g1, 0.001)
+print("g''(10)  = {}".format(g2(10)))
+
+g3 = derivative(g2, 0.001)
+(g2, 0.001)
+print("g'''(10) = {}".format(g3(10)))
+
+print()
+
+print("g'''(10) = {}".format(kthDerivative(g, 0.001, 3)(10)))
+
+
+def h(x):
+    return x**2
+
+
+print("h'''(10) = {}".format(kthDerivative(h, 0.001, 4)(10)))
