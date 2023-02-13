e618d5aa0e54ddb307654107f9e586b11e11fe25
John Bleichert <omortis@outlook.com>
2023-02-13 11:28:45 -0500

    changes to gaz

--- sandbox/gaz.py
+++ sandbox/gaz.py
@@ -1,5 +1,4 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from gazpacho import get, Soup
 import os
 
 if os.name == "posix":
@@ -7,13 +6,24 @@ if os.name == "posix":
 else:
     os.system("cls")
 
+# html = get(url)
+# soup = Soup(html)
+# books = soup.find('div', {'class': 'book-'}, partial=True)
+
+# def parse(book):
+#     name = book.find('h4').text
+#     price = float(book.find('p').text[1:].split(' ')[0])
+#     return name, price
+
+# [parse(book) for book in books]
+
 # Sample: https://diy.sndimg.com/content/dam/images/diy/fullset/2011/1/20/0/DIY_art-deco-home-exterior_s3x4.jpg.rend.hgtvcom.616.822.suffix/1420692209594.jpeg
 url = "https://www.hgtv.com/design/decorating/design-101/popular-architectural-home-styles-pictures"
 
-link = urlopen(url).read()
-soup = BeautifulSoup(link, "html.parser")
+html = get(url)
+soup = Soup(html)
 
-imgList = soup.findAll("img")
+imgList = soup.find("img")
 
 for i in imgList:
     print()
