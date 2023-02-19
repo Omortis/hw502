295247e21fbece2b531d6d8cb091c657e5cff2ac
John Bleichert <omortis@outlook.com>
2023-02-19 10:57:25 -0500

    ps03 complete

--- ps03/ex6.py
+++ ps03/ex6.py
@@ -14,9 +14,11 @@ class House(object):
         self.numUnits = numUnits
         self.numFloors = numFloors
         self.marketValue = marketValue
-        self._doorsLocked = False  # protected
-        self._windowsClosed = True  # protected
-        self._coffeeMakerOn = False  # protected
+        self._doorsLocked = False
+        self._windowsClosed = True
+        self._coffeeMakerOn = False
+        self._intruderAlert = False
+        self._seaBorneMenace = False
 
     def __str__(self):
         output = self.address + ", "
@@ -31,6 +33,7 @@ class House(object):
     def __lt__(self, other):
         return self.marketValue < other.marketValue
 
+    # -------------------------------
     def areDoorsLocked(self):
         return self._doorsLocked
 
@@ -40,6 +43,7 @@ class House(object):
     def unlockDoors(self):
         self._doorsLocked = False
 
+    # -------------------------------
     def areWindowsClosed(self):
         return self._windowsClosed
 
@@ -50,6 +54,7 @@ class House(object):
         self._windowsClosed = False
 
     # Added for 6b.
+    # -------------------------------
     def turnCoffeeOn(self):
         self._coffeeMakerOn = True
 
@@ -58,6 +63,26 @@ class House(object):
 
     def isCoffeeOn(self):
         return self._coffeeMakerOn
+
+    # -------------------------------
+    def releaseTheKraken(self):
+        self._seaBorneMenace = False
+
+    def invadersApproachBySea(self):
+        self._seaBorneMenace = True
+
+    def seaBorneMenace(self):
+        return self._seaBorneMenace
+
+    # -------------------------------
+    def releaseTheHounds(self):
+        self._intruderAlert = False
+
+    def yellIntruderAlert(self):
+        self._intruderAlert = True
+
+    def intruderAlert(self):
+        return self._intruderAlert
 
 
 home = House("1313 Mockingbird Lane", "Colonial", "1929", 1, 3, 125000)
@@ -92,3 +117,25 @@ assert home.isCoffeeOn() == False
 home.turnCoffeeOn()
 print("Is there coffee on?   ", home.isCoffeeOn())
 assert home.isCoffeeOn() == True
+
+print("\nseaBorneMenace()")
+print("Are we being invaded by sea?   ", home.seaBorneMenace())
+assert home.seaBorneMenace() == False
+home.invadersApproachBySea()
+print("Are we being invaded by sea?   ", home.seaBorneMenace())
+print("Release the kraken!!!")
+assert home.seaBorneMenace() == True
+home.releaseTheKraken()
+print("Are we being invaded by sea?   ", home.seaBorneMenace())
+assert home.seaBorneMenace() == False
+
+print("\nintruderAlert()")
+print("Is there an intruder alert?   ", home.intruderAlert())
+assert home.intruderAlert() == False
+home.yellIntruderAlert()
+print("Is there an intruder alert?   ", home.intruderAlert())
+assert home.intruderAlert() == True
+print("Release the hounds!!!")
+home.releaseTheHounds()
+print("Is there an intruder alert?   ", home.intruderAlert())
+assert home.intruderAlert() == False
