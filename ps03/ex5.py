87f9dba7ee7f68bf2117d9d1c7f886e29886cad8
John Bleichert <omortis@outlook.com>
2023-02-18 23:29:23 -0500

    ex5 got f() working

--- ps03/ex5.py
+++ ps03/ex5.py
@@ -19,43 +19,40 @@ else:
 # ans = argTest(5, 7)(6)
 # print("ans: ", ans)
 
-def argTest(coeffs):
+# def argTest(coeffs):
+#     if len(coeffs) == 1:
+#         def f(z):
+#             return coeffs[0]
+#         return f
+#     else:
+#         def f(z):
+#             exponent = len(coeffs) - 1
+#             return coeffs[-1] * (z**exponent) + argTest(coeffs[:-1])(z)
+#         return f
 
-    print("coeffs     : ", coeffs)
-    print("len(coeffs): ", len(coeffs))
 
+# ans = argTest([1, 2, 1])(5)
+# print("ans: ", ans)
+
+# assert abs(argTest([1, 2, 1])(5) - 36) <= .1
+# assert abs(argTest([1, 1, 1])(5) - 31) <= .1
+# assert abs(argTest([1, 2, 3])(4) - 57) <= .1
+
+
+def myPoly(coeffs):
+    """coeffs is a list of floats or ints, function returns a polynomial with
+    the kth entry in coeffs as the coefficient of the monomial x^k"""
     if len(coeffs) == 1:
-        print("\tcase 1")
-        return coeffs[0]
-
+        def f(z):
+            return coeffs[0]
+        return f
     else:
         def f(z):
             exponent = len(coeffs) - 1
-            print("\tcoeffs[-1]: ", coeffs[-1])
-            print("\texponent  : ", exponent)
-            return coeffs[-1] * (z**exponent) + argTest(coeffs[:-1])(z)
+            return coeffs[-1] * (z**exponent) + myPoly(coeffs[:-1])(z)
         return f
 
 
-ans = argTest([1, 2, 1])(5)
-print("ans: ", ans)
-
-
-# def myPoly(coeffs):
-#     """coeffs is a list of floats or ints, function returns a polynomial with
-#     the kth entry in coeffs as the coefficient of the monomial x^k"""
-#     print("\ncoeffs: ", coeffs)
-
-#     if len(coeffs) == 1:
-#         print("+ ", coeffs[0])
-#         return coeffs[0]
-#     else:
-#         print("last: ", coeffs[-1])
-#         print(str(coeffs[-1]) + "*x**" + str(len(coeffs) - 1))
-#         print("coeffs[:-1]: ", coeffs[:-1])
-#         return coeffs[-1] * myPoly(coeffs[:-1])
-
-
-# ans = myPoly([1, 2, 1])
-
-# print("ans: ", ans)
+assert abs(myPoly([1, 2, 1])(5) - 36) <= .1
+assert abs(myPoly([1, 1, 1])(5) - 31) <= .1
+assert abs(myPoly([1, 2, 3])(4) - 57) <= .1
